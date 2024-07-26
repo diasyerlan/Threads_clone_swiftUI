5e52911bf35537676eefda64064a5d8009a54bfc
diasyerlan <git config --global user.email diasyerlan@gmail.com>
2024-07-26 19:13:55 +0500

    Project setup done

---
+++ ThreadsClone/App/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Dias Yerlan on 26.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThreadsClone/Core/Authentication/VIews/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  ThreadsClone
+//
+//  Created by Dias Yerlan on 26.07.2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    LoginView()
+}
