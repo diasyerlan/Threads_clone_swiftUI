0fa9590c85fa28cb7b0cba2cd6b7d49e641361d0
diasyerlan <git config --global user.email diasyerlan@gmail.com>
2024-07-28 09:34:25 +0500

    Feed View UI created

--- ThreadsClone/Core/Feed/Views/FeedView.swift
+++ ThreadsClone/Core/Feed/Views/FeedView.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        Text("Feed View")
+        NavigationStack {
+            ScrollView(showsIndicators: false) {
+                LazyVStack {
+                    ForEach(0 ..< 10, id: \.self) { thread in
+                        ThreadCell()
+                    }
+                }
+            }
+            .refreshable {
+                
+            }
+            .navigationTitle("Threads")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Image(systemName: "arrow.counterclockwise")
+                }
+            }
+        }
     }
 }
 
---
+++ ThreadsClone/Core/Feed/Views/ThreadCell.swift
@@ -0,0 +1,74 @@
+//
+//  ThreadCell.swift
+//  ThreadsClone
+//
+//  Created by Dias Yerlan on 28.07.2024.
+//
+
+import SwiftUI
+
+struct ThreadCell: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top, spacing: 12) {
+                Image("person")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 40, height: 40)
+                    .background(Color(.systemGray5))
+                    .clipShape(Circle())
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    HStack {
+                        Text("Patrick Bateman")
+                            .fontWeight(.semibold)
+                        Spacer()
+                        
+                        Text("10 min")
+                            .font(.caption)
+                            .foregroundStyle(Color(.systemGray3))
+                        
+                        Image(systemName: "ellipsis")
+                            .foregroundColor(Color(.darkGray))
+                        
+                    }
+                        
+                    Text("You know who I am")
+                    
+                    HStack(spacing: 16) {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "heart")
+                        }
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "bubble.right")
+                        }
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "arrow.rectanglepath")
+                        }
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "paperplane")
+                        }
+                    }
+                    .foregroundStyle(.black)
+                    .padding(.vertical, 8)
+                }
+                .font(.footnote)
+                
+            }
+            Divider()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ThreadCell()
+}
